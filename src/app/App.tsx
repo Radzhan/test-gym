6337e0daea80268a90ac409b6c28671d4290862e
Radzhan <rbayhombaev@gmail.com>
2024-05-19 12:32:16 +0600

    add routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
-
+import { Route, Routes } from "react-router-dom";
+import { MainPage } from "../pages/MainPage";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="*" element={<h2>Not Found !</h2>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
